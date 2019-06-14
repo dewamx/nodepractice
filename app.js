b5a6f1255cbfe4194e64106715c96e5d6fb9ee56
raxemmohamed <raxemmohamed@gmail.com>
2019-06-14 09:26:59 -0500

    test

---
+++ app.js
@@ -0,0 +1,7 @@
+let noun1 = 'Raxem';
+let adjective = 'funny';
+let noun2 = 'dude';
+let verb = 'laugh';
+let noun3 = 'fruit';
+
+console.log('The first ' + noun1 + ' was a very ' + adjective + ' ' + noun2 + ' who loved to ' + verb + ' while eating ' + noun3 + '.');
